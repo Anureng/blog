985dd4b805195c77be2350f182bb862efb3d5433
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-06-25 16:17:49 +0530

    done

--- src/app/api/blog/getBlogData/route.ts
+++ src/app/api/blog/getBlogData/route.ts
@@ -4,24 +4,28 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
     try {
-        const getData = await redis.get("post")
-        if (getData) {
-            return NextResponse.json({ getData })
+        const cacheKey = "post";
+        const MAX_AGE = 60 * 60; // 1 hour in seconds
+
+        // Try to fetch the data from Redis cache
+        const cachedData = await redis.get(cacheKey);
+        if (cachedData) {
+            // If data exists in the cache, return it
+            return NextResponse.json(cachedData);
         }
+
+        // If cache is empty, fetch the data from the database
         const posts = await client.post.findMany({
             include: {
                 comment: true, // Include related comments if needed
             },
         });
 
+        // Store the fetched data in Redis cache with an expiration time
+        await redis.set(cacheKey, JSON.stringify(posts), { ex: MAX_AGE });
 
-        const MAX_AGE = 60_000 * 60;
-
-
-        await redis.set("post", JSON.stringify(posts), { ex: MAX_AGE })
-
-        return new NextResponse(JSON.stringify(posts), { status: 200 });
-
+        // Return the fetched data
+        return NextResponse.json(posts);
     } catch (error: any) {
         console.error(error);
         return new NextResponse(error.message, { status: 500 });
