6ceaf187bbf5cd3417bc633abaad493b18e5f376
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-06-30 17:52:39 +0530

    do

--- src/app/(router)/AI/page.tsx
+++ src/app/(router)/AI/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
             <iframe
                 src="https://www.chatbase.co/chatbot-iframe/ibApmgDomCQUQQuaI3cA0"
                 width="100%"
+                height="100%"
             ></iframe>
         </div >
     )
