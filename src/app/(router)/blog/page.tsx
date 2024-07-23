efcec60a6d5b29e6ab7c7e89453db93c124eba67
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-07-23 17:52:07 +0530

    solved

--- src/app/(router)/blog/page.tsx
+++ src/app/(router)/blog/page.tsx
@@ -1,25 +1,34 @@
+// app/blog/page.jsx
 
-import AllData from '@/app/Components/AllData';
-import Navbar from '@/app/Components/Navbar';
 import React from 'react';
+import Navbar from '@/app/Components/Navbar';
+import AllData from '@/app/Components/AllData';
 
 const fetchBlogData = async () => {
-
-    const res = await fetch("https://blog-gold-beta.vercel.app/api/blog/getBlogData");
+    const res = await fetch(`https://blog-gold-beta.vercel.app/api/blog/getBlogData`, {
+        cache: 'no-store',
+    });
     if (!res.ok) {
         throw new Error('Failed to fetch data');
     }
     return res.json();
 };
 
-const Page = async () => {
-    const data = await fetchBlogData();
+const BlogPage = async () => {
+    let data;
+    try {
+        data = await fetchBlogData();
+    } catch (error) {
+        console.error('Error fetching blog data:', error);
+        data = null;
+    }
+
     return (
         <>
             <Navbar />
-            <AllData data={data} />
+            {data ? <AllData data={data} /> : <p>Error fetching data</p>}
         </>
     );
 };
 
-export default Page;
+export default BlogPage;
